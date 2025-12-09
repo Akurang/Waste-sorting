5ab22e70a20fcae0c83317af033f3d7a90228327
Akurang <gabakurang@gmail.com>
2025-12-09 21:29:37 +0000

    Updated version

---
+++ Organic.java
@@ -0,0 +1,52 @@
+import java.util.ArrayList;
+
+public class Organic extends Waste implements Recycable{
+
+    private boolean isCompostable;
+    private String type;
+   
+    private ArrayList<Organic> organicList = new ArrayList<>();
+
+    
+    //constructors
+
+    Organic(boolean isCompostable, String type ){
+        super(types.ORGANIC);
+        this.type = type;
+        this.isCompostable = isCompostable;
+    }
+
+    //getters
+
+    public String getType(){
+        return type;
+    }
+
+    public boolean getIsCompostable(){
+        return isCompostable;
+    }
+
+    //setters
+
+    public void setType(String type){
+        this.type = type;
+    }
+
+    public void setIsCompostable(boolean isCompostable){
+        this.isCompostable = isCompostable;
+    }
+
+    // Add an Organic item to the list
+    public void addOrganic(Organic o) {
+        organicList.add(o);
+    }
+
+    @Override
+
+    public boolean isRecycable(){
+        return true;
+    }
+
+    
+
+}
